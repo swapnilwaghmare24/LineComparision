50df25174b1e784eab3b0469253e8ca79740e393
swapnilwaghmare24 <iamswapnilengg@gmail.com>
2022-05-02 22:33:22 +0530

    .Ignore and first line committed

---
+++ LineComparision.java
@@ -0,0 +1,12 @@
+package LineComparision;
+
+import java.sql.SQLOutput;
+
+public class LineComparision
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to line comparision computation program" );
+    }
+
+}
